024603a4f5162caa68cef23e5348ba586dc119be
Nisus-Liu <609069481@qq.com>
2020-05-29 23:48:10 +0800

    fixup insert

--- basic/redback_tree.go
+++ basic/redback_tree.go
@@ -4,25 +4,24 @@ import (
 	"fmt"
 )
 
-
-
 type NodeColor uint8
+
 const (
-	Red NodeColor = iota
+	Red   NodeColor = iota
 	Black NodeColor = iota
 )
-
 
 // EMPTY 空节点, 叶子节点标识
 // 空节点是黑色
 type NilNode = Node
+
 var EMPTY = &NilNode{Color: Black}
 
 type Node struct {
-	Key int64
-	Color NodeColor
-	Left *Node
-	Right *Node
+	Key    int64
+	Color  NodeColor
+	Left   *Node
+	Right  *Node
 	Parent *Node
 }
 
@@ -33,11 +32,11 @@ func (n *Node) GetRight() interface{} {
 	return n.Right
 }
 
-func NewNode(key int64, color NodeColor , left *Node, right *Node, parent *Node) *Node {
-	if(&color== nil) {
+func NewNode(key int64, color NodeColor, left *Node, right *Node, parent *Node) *Node {
+	if &color == nil {
 		color = Red
 	}
-	if(left== nil) {
+	if left == nil {
 		left = EMPTY
 	}
 	if right == nil {
@@ -46,14 +45,14 @@ func NewNode(key int64, color NodeColor , left *Node, right *Node, parent *Node)
 	if parent == nil {
 		parent = EMPTY
 	}
-	return &Node{Key: key, Color:color, Left: left, Right: right, Parent: parent}
+	return &Node{Key: key, Color: color, Left: left, Right: right, Parent: parent}
 }
 
-func NewNode2(key int64, color NodeColor) *Node{
+func NewNode2(key int64, color NodeColor) *Node {
 	return NewNode(key, color, EMPTY, EMPTY, EMPTY)
 }
 
-func (t *Node) String() string{
+func (t *Node) String() string {
 	return fmt.Sprintf("key: %d, color: %d, left.key: %d, right.key: %d",
 		t.Key, t.Color, t.Left.Key, t.Right.Key)
 	// return fmt.Sprintf("key: %d, color: %d, left: %s, right: %s",
@@ -62,9 +61,7 @@ func (t *Node) String() string{
 
 // RBTree 红黑树
 type RBTree struct {
-
 	root *Node
-
 }
 
 func NewRBTree() *RBTree {
@@ -75,25 +72,27 @@ func (r *RBTree) GetRoot() *Node {
 	return r.root
 }
 
-func (t *RBTree) Insert(node *Node) *RBTree{
-	return t.insert0(t.root, node)
+func (t *RBTree) Insert(node *Node) *RBTree {
+	t.insert0(t.root, node)
+	t.insertFixUp(node)
+	return t
 }
 
 func (t *RBTree) insert0(root *Node, node *Node) *RBTree {
 	var nextNode *Node = nil
-	if(root== EMPTY) {
+	if root == EMPTY {
 		// 还是个空树, 当前节点作为根节点
 		t.root = node
 		return t
-	}else if node.Key < root.Key {
-		if(root.Left == EMPTY) {
+	} else if node.Key < root.Key {
+		if root.Left == EMPTY {
 			node.Parent = root
 			root.Left = node
 			return t
 		}
 		nextNode = root.Left
-	}else if node.Key >= root.Key {
-		if(root.Right == EMPTY) {
+	} else if node.Key >= root.Key {
+		if root.Right == EMPTY {
 			node.Parent = root
 			root.Right = node
 			return t
@@ -119,9 +118,9 @@ func (t *RBTree) LeftRotate(x *Node) {
 	// y 是作为左还是右孩子? 还是根? 取决于x之前是什么
 	if x.Parent == EMPTY { // x 是根 => y 要作为新根
 		t.root = y
-	}else if x == x.Parent.Left { // x 是左 => y 左
+	} else if x == x.Parent.Left { // x 是左 => y 左
 		x.Parent.Left = y
-	}else {
+	} else {
 		x.Parent.Right = y
 	}
 
@@ -144,9 +143,9 @@ func (t *RBTree) RightRotate(x *Node) {
 	// y 是作为左还是右孩子? 还是根? 取决于x之前是什么
 	if x.Parent == EMPTY { // x 是根 => y 要作为新根
 		t.root = y
-	}else if x == x.Parent.Left { // x 是左 => y 左
+	} else if x == x.Parent.Left { // x 是左 => y 左
 		x.Parent.Left = y
-	}else {
+	} else {
 		x.Parent.Right = y
 	}
 
@@ -165,21 +164,21 @@ func (t *RBTree) insertFixUp(z *Node) {
 				y.Color = Black
 				z.Parent.Parent.Color = Red
 				z = z.Parent.Parent
-			}else if z == z.Parent.Right { // 先左旋, 再右旋
+			} else if z == z.Parent.Right { // 先左旋, 再右旋
 				z = z.Parent
 				t.LeftRotate(z)
 			}
 			z.Parent.Color = Black
 			z.Parent.Parent.Color = Red
 			t.RightRotate(z.Parent.Parent)
-		}else { // 对称
+		} else { // 对称
 			y := z.Parent.Parent.Left
 			if y.Color == Red {
 				z.Parent.Color = Black
 				y.Color = Black
 				z.Parent.Parent.Color = Red
 				z = z.Parent.Parent
-			}else if z == z.Parent.Left {
+			} else if z == z.Parent.Left {
 				z = z.Parent
 				t.RightRotate(z)
 			}
@@ -191,6 +190,6 @@ func (t *RBTree) insertFixUp(z *Node) {
 	t.root.Color = Black
 }
 
-func (t * RBTree) String() string {
+func (t *RBTree) String() string {
 	return t.root.String()
 }
